2d2e49810a3b34a6eb26481ccc52c81d4f595057
laza83 <41151943+laza83@users.noreply.github.com>
2018-11-29 20:14:13 +0100

    Modified selection and collision box
    Code modified to support additional materials.

---
+++ materials.lua
@@ -0,0 +1,195 @@
+local nici = 1
+-- Default Materials
+
+--default:stone
+mymillwork.register("default:stone",
+    "default_stone",
+    "Stone",
+    "default_stone.png",
+    {cracky=3, stone=1, not_in_creative_inventory=nici}
+)
+
+--default:cobble
+mymillwork.register("default:cobble",
+    "default_cobble",
+    "Cobblestone",
+    "default_cobble.png",
+    {cracky=3, stone=2, not_in_creative_inventory=nici}
+)
+
+--default:stonebrick
+mymillwork.register("default:stonebrick",
+    "default_stone_brick",
+    "Stone Brick",
+    "default_stone_brick.png",
+    {cracky=2, stone=1, not_in_creative_inventory=nici}
+)
+
+--default:stone_block
+
+--default:mossycobble
+mymillwork.register("default:mossycobble",
+    "default_mossy_cobble",
+    "Mossy Cobble",
+    "default_mossycobble.png",
+    {cracky=3, stone=1, not_in_creative_inventory=nici}
+)
+
+--default:desert_stone
+mymillwork.register("default:desert_stone",
+    "default_desert_stone",
+    "Desert Stone",
+    "default_desert_stone.png",
+    {cracky=3, stone=1, not_in_creative_inventory=nici}
+)
+
+--default:desert_cobble
+--default:desert_stonebrick
+--default:desert_stone_block
+
+--default:sandstone
+mymillwork.register("default:sandstone",
+    "default_sandstone",
+    "Sandstone",
+    "default_sandstone.png",
+    {crumbly=1, cracky = 3, not_in_creative_inventory=nici}
+)
+
+--default:sandstonebrick
+--default:sandstone_block
+
+--default:desert_sandstone
+mymillwork.register("default:desert_sandstone",
+    "default_desert_sandstone",
+    "Desert Sandstone",
+    "default_desert_sandstone.png",
+    {crumbly=1, cracky = 3, not_in_creative_inventory=nici}
+)
+
+--default:desert_sandstone_brick
+--default:desert_sandstone_block
+
+--default:silver_sandstone
+mymillwork.register("default:silver_sandstone",
+    "default_silver_sandstone",
+    "Silver Sandstone",
+    "default_silver_sandstone.png",
+    {crumbly=1, cracky = 3, not_in_creative_inventory=nici}
+)
+
+--default:silver_sandstone_brick
+--default:silver_sandstone_block
+
+--default:obsidian
+mymillwork.register("default:obsidian",
+    "default_obsidian",
+    "Obsidian",
+    "default_obsidian.png",
+    {cracky=1, level=2, not_in_creative_inventory=nici}
+)
+
+--default:obsidianbrick
+--default:obsidian_block
+
+--default:clay
+mymillwork.register("default:clay",
+    "default_clay",
+    "Clay",
+    "default_clay.png",
+    {crumbly=3, not_in_creative_inventory=nici}
+)
+
+--default:snow
+--default:snowblock
+--default:ice
+
+--default:tree
+
+--default:wood
+mymillwork.register("default:wood",
+    "default_wood",
+    "Wood",
+    "default_wood.png",
+    {choppy=2, oddly_breakable_by_hand=2, flammable=2, wood=1, not_in_creative_inventory=nici}
+)
+
+--default:jungletree
+
+--default:junglewood
+mymillwork.register("default:junglewood",
+    "default_junglewood",
+    "Jungle Wood",
+    "default_junglewood.png",
+    {choppy=2, oddly_breakable_by_hand=2, flammable=2, wood=1, not_in_creative_inventory=nici}
+)
+
+--default:pine_tree
+
+--default:pine_wood
+mymillwork.register("default:pine_wood",
+    "default_pine_wood",
+    "Pine Wood",
+    "default_pine_wood.png",
+    {choppy=3, oddly_breakable_by_hand=2, flammable=3, wood=1, not_in_creative_inventory=nici}
+)
+
+--default:acacia_tree
+
+--default:acacia_wood
+mymillwork.register("default:acacia_wood",
+    "default_acacia_wood",
+    "Acacia Wood",
+    "default_acacia_wood.png",
+    {choppy=2, oddly_breakable_by_hand=2, flammable=2, wood=1, not_in_creative_inventory=nici}
+)
+
+--default:aspen_tree
+
+--default:aspen_wood
+mymillwork.register("default:aspen_wood",
+    "default_aspen_wood",
+    "Aspen Wood",
+    "default_aspen_wood.png",
+    {choppy=3, oddly_breakable_by_hand=2, flammable=3, wood=1, not_in_creative_inventory=nici}
+)
+
+--default:coalblock
+mymillwork.register("default:coalblock",
+    "default_coal_block",
+    "Coal Block",
+    "default_coal_block.png",
+    {cracky=3, not_in_creative_inventory=nici}
+)
+
+--default:steelblock
+--default:copperblock
+--default:tinblock
+--default:bronzeblock
+--default:goldblock
+--default:mese
+
+--default:diamondblock
+mymillwork.register("default:diamondblock",
+    "default_diamond_block",
+    "Diamond Block",
+    "default_diamond_block.png",
+    {cracky=1, level=3, not_in_creative_inventory=nici}
+)
+
+--default:glass
+mymillwork.register("default:glass",
+    "default_glass",
+    "Glass",
+    "default_glass.png",
+    {cracky=3, oddly_breakable_by_hand=3, not_in_creative_inventory=nici}
+)
+
+--default:obsidian_glass
+
+--default:brick
+mymillwork.register("default:brick",
+    "default_brick",
+    "Brick",
+    "default_brick.png",
+    {cracky=3, not_in_creative_inventory=nici}
+)
